111184a27dc778f0bc0488c3ea604e9fc4e107a0
hassenehadjour98 <hassenehadjour98@gmail.com>
2022-12-30 14:21:54 -0500

    name of the topic

--- src/datastructure/DataReader.java
+++ src/datastructure/DataReader.java
@@ -29,7 +29,7 @@ public class DataReader {
 		 * Use For Each loop/while loop/Iterator to retrieve data.
 		 */
 
-		String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car.txt";
+//		String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car.txt";
 
 		// specify the path to the text file
 		Path filePath = Paths.get("user.dir" + "/src/data/self-driving-car.txt");
